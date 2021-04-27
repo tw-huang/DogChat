9866fd2cff42007cf4aeb1236f984b0f545dc58d
tw.huang <tw.huang@foxmail.com>
2021-04-27 16:50:22 +0800

    #init#

---
+++ src/test/java/me/twhuang/dogchat/DogchatApplicationTests.java
@@ -0,0 +1,13 @@
+package me.twhuang.dogchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
