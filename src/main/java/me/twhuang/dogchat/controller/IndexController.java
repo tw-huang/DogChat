bfaf407652eaf33c9a0faef2d91af0210d203411
tw.huang <tw.huang@foxmail.com>
2021-05-08 10:43:01 +0800

    #登录#注册页面#

--- src/main/java/me/twhuang/dogchat/controller/IndexController.java
+++ src/main/java/me/twhuang/dogchat/controller/IndexController.java
@@ -16,4 +16,15 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping(value = "/register")
+    public String register() {
+        return "register";
+    }
+
 }
