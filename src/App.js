9a8c6fe758aac37c1cac19f68ba1a91f14876ee7
tw.huang <tw.huang@foxmail.com>
2021-05-19 16:54:49 +0800

    #redux#

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import Img from "./assets/640.png";
 
 class App extends Component {
     render() {
         return (
-            <div>
-                hello
+            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+                <div className="flex-shrink-0">
+                    <img className="h-12 w-12" src={Img} alt=""/>
+                </div>
+                <div>
+                    <div className="text-xl font-medium text-black">ChitChat</div>
+                    <p className="text-gray-500">You have a new message!</p>
+                </div>
             </div>
         );
     }
